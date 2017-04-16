e34d1ae5c841533136afb272d45c377b0ba181bd
Ravali Ginjupalli <ravali.ginjupalli@gmail.com>
2017-04-16 01:17:32 -0500

    Add attribute value pairs

---
+++ attr_value_pairs.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+av_pairs = {}
+
+def add_to_avpair(a, v, i):
+
+	if((a, v) not in av_pairs.keys()):
+		av_pairs[(a, v)] = []
+		av_pairs[(a, v)].append(i)
+	else:
+		av_pairs[(a, v)].append(i)
+
+
+def calculate_av_pairs(attributes, table):
+	inconsistent = set(['*', '?', '-'])
+	d_values = table[:,len(attributes)-1]
+
+	num_attr = len(attributes) - 1
+	for attr in range(0, num_attr):
+
+		a = attributes[attr]
+		a_values = table[:,attr]
+		a_unique_val = list(set(a_values) - inconsistent)
+
+
+		for i in range(0, len(a_values)):
+			if(a_values[i] not in inconsistent):
+				add_to_avpair(a, a_values[i], i)
+			elif(a_values[i] == '?'):
+				pass
+			elif(a_values[i] == '*'):
+				for j in range(0, len(a_unique_val)):
+					add_to_avpair(a, a_unique_val[j], i)
+			elif(a_values[i] == '-'):
+
+				d = table[i,len(attributes)-1]
+				val_set = set()
+
+				for k in range(0, len(d_values)):
+					if(d == d_values[k]):
+						print(d_values[k])
+						val = table[k,attr]				# Replace wind attribute 0 with corresponding value
+						if(val not in inconsistent):
+							val_set.add(val)
+			
+				for x in val_set:
+					add_to_avpair(a, x, i)		
+
+	print(av_pairs)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
 from table import createTable
+from attr_value_pairs import calculate_av_pairs, add_to_avpair
 
 file = 'input1.txt'
 
 table, attributes = createTable(file)
-print(table)
+
+
+calculate_av_pairs(attributes, table)
 
 
